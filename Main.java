73b8f79640d1d427b0b69651e1eda3183b8d1666
Alexander <137441604+alex74hzn@users.noreply.github.com>
2023-10-13 11:17:50 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,12 +2,22 @@ package com.example.media;
 
 public class Main {
     public static void main(String[] args) {
-        MediaFile mp3 =  new MP3("MP3", 5, "Ля-ля-ля");
+               MediaFile mp3 =  new MP3("MP3", 5, "Ля-ля-ля", 256);
         MediaFile video = new Video("video", 12, "  Кадр ");
         MediaFile podcast = new Podcast("podcast", 8, "Бла-бла");
         mp3.play();
-        video.play();
         mp3.pause();
-        
+        String string = mp3.toString();
+        System.out.println(string);
+
+        video.play();
+        video.pause();
+        video.rewind();
+        System.out.println(video);
+
+        podcast.play();
+        podcast.pause();
+        podcast.rewind();
+        System.out.println(podcast);
     }
 }
